e5eb5a5e56d7f1808aa76ab7d4817b413f1a39df
Alexander Vagner <onemybox@gmail.com>
2021-10-15 10:01:59 +0300

    create telegram notification

---
+++ telegram_notify.py
@@ -0,0 +1,25 @@
+import os
+import traceback
+import warnings
+
+import telebot
+from luigi import Task
+
+CHAT_ID = '327639112'
+BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
+
+
+def send_notification(task: Task, exception: Exception):
+    if BOT_TOKEN is None:
+        warnings.warn('TG_BOT_TOKEN is not set')
+        # если не задан токен в переменной окружения, то просто выходим, чтобы не ломать
+        # работу пайплайна
+        return
+
+    bot = telebot.TeleBot(BOT_TOKEN)
+    text = (
+        f'Упала задача: {task.__class__.__name__}\n'
+        f'Исключение: {exception.__class__.__name__}, {traceback.format_exc()}'
+    )
+    bot.send_message(CHAT_ID, text)
+
